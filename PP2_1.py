55035d4fab7b9eca7e680522b74a06344d2e52fd
Johnny-Zhao-ICD2O1-01 <johnny.zhao@student.tdsb.on.ca>
2024-10-09 13:51:40 +0000

    commit

--- PP2_1.py
+++ PP2_1.py
@@ -1,16 +1,19 @@
-
-
 def q1(): 
-  #Write Assignment code here
-
+  numQ1 = int(input("Input an integer: "))
+  if numQ1 % 2 == 0:
+    print(f"In: {numQ1}\n{numQ1} is even")
+  else:
+    print(f"In: {numQ1}\n{numQ1} is odd")
 
 def q2(): 
-  #Write Assignment code here
-
-
+  nameQ2 = str(input("Input a name: "))
+  if nameQ2 == "Kalisz":
+    print(f"In: {nameQ2}\nteacher")
+  else:
+    print(f"In: {nameQ2}\nstudent")
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+#q1()
+#2()
