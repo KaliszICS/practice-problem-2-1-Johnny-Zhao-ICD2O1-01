0e519fb2ba0dabdc953e0e62ac11125ff1337d74
Johnny-Zhao-ICD2O1-01 <johnny.zhao@student.tdsb.on.ca>
2024-10-09 14:03:40 +0000

    commit

--- PP2_1.py
+++ PP2_1.py
@@ -1,19 +1,26 @@
+'''
+    Practice Questions: If statments
+    Author: Johnny Zhao
+    Date Created: Oct 9, 2024
+    Date Last Modified: Oct 9, 2024
+'''
+
 def q1(): 
-  numQ1 = int(input("Input an integer: "))
-  if numQ1 % 2 == 0:
-    print(f"In: {numQ1}\n{numQ1} is even")
-  else:
-    print(f"In: {numQ1}\n{numQ1} is odd")
+  numQ1 = int(input("In: ")) #Asking the user to input a integer
+  if numQ1 % 2 == 0: #division to see if there's a remainder(to see if it's even)
+    print(f"{numQ1} is even") #outputting "_their integer_ is even"
+  else: #if the integer is not even(odd)
+    print(f"{numQ1} is odd") #outputting "_their integer_ is even"
 
 def q2(): 
-  nameQ2 = str(input("Input a name: "))
-  if nameQ2 == "Kalisz":
-    print(f"In: {nameQ2}\nteacher")
-  else:
-    print(f"In: {nameQ2}\nstudent")
+  nameQ2 = str(input("In: ")) #Asking the user to input a name
+  if nameQ2 == "Kalisz": #test is the name is "Kalisz"
+    print(f"teacher") #outputting "teacher"
+  else: #if the name is not "Kalisz"
+    print(f"student") #outputting "student"
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
 #q1()
-#2()
+#q2()
